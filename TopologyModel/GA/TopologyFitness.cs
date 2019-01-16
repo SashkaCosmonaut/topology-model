e6fa3afbf8501fb6a4a1c647665a3305e05312b2
Alexander Sokolov <alexander.sokolov.it@gmail.com>
2019-01-16 19:03:34 +0400

    Добавлен класс фитнес-функции для топологии.

---
+++ TopologyModel/GA/TopologyFitness.cs
@@ -0,0 +1,18 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Fitnesses;
+
+namespace TopologyModel.GA
+{
+	/// <summary>
+	/// Класс фитнес-функции для топологии.
+	/// </summary>
+	public class TopologyFitness : IFitness
+	{
+		/// <summary>
+		/// Рассчитать приспособленность хромосомы.
+		/// </summary>
+		/// <param name="chromosome"></param>
+		/// <returns></returns>
+		public double Evaluate(IChromosome chromosome) => throw new System.NotImplementedException();
+	}
+}
