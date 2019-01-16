b25a52c9928aa4f45735b8e52ee874ce79f39dc1
Alexander Sokolov <alexander.sokolov.it@gmail.com>
2019-01-16 19:15:27 +0400

    Добавлен код инициализации и запуска ГА.

--- TopologyModel/Program.cs
+++ TopologyModel/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace TopologyModel
+﻿using GeneticSharp.Domain;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Domain.Terminations;
+using System;
+using TopologyModel.GA;
+
+namespace TopologyModel
 {
 	public class Program
 	{
@@ -9,6 +18,23 @@
 
 		public static void Main(string[] args)
 		{
+			var selection = new EliteSelection();
+			var crossover = new OrderedCrossover();
+			var mutation = new ReverseSequenceMutation();
+			var fitness = new TopologyFitness();
+			var chromosome = new TopologyChromosome();
+			var population = new Population(50, 70, chromosome);
+
+			var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation)
+			{
+				Termination = new GenerationNumberTermination(100)
+			};
+
+			Console.WriteLine("GA running...");
+
+			ga.Start();
+
+			Console.WriteLine("Best solution found has {0} fitness", ga.BestChromosome.Fitness);
 		}
 	}
 }
