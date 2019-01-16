fd2044ee1f5df0931216076487b10471ca3b5d27
Alexander Sokolov <alexander.sokolov.it@gmail.com>
2019-01-16 19:15:04 +0400

    Добавлен класс хромосомы топологии.

---
+++ TopologyModel/GA/TopologyChromosome .cs
@@ -0,0 +1,34 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+
+namespace TopologyModel.GA
+{
+	/// <summary>
+	/// Класс хромосомы топологии.
+	/// </summary>
+	public class TopologyChromosome : ChromosomeBase
+	{
+		/// <summary>
+		/// Change the argument value passed to base construtor to change the length of your chromosome.
+		/// </summary>
+		public TopologyChromosome() : base(10)
+		{
+			CreateGenes();
+		}
+
+		/// <summary>
+		/// Generate a gene base on my problem chromosome representation.
+		/// </summary>
+		/// <param name="geneIndex"></param>
+		/// <returns></returns>
+		public override Gene GenerateGene(int geneIndex) => throw new System.NotImplementedException();
+
+		/// <summary>
+		/// Создать новую хромосому текущего класса.
+		/// </summary>
+		/// <returns>Новый объект текущего класса (хромосомы).</returns>
+		public override IChromosome CreateNew()
+		{
+			return new TopologyChromosome();
+		}
+	}
+}
