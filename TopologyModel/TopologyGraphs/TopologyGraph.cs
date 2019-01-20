faf198a5b89ccb2814448fe0dbb50cfba64e25e3
Alexander <sashkacosmonaut@gmail.com>
2019-01-20 14:48:16 +0400

    Добавлены классы графа и вершины графа.

---
+++ TopologyModel/TopologyGraphs/TopologyGraph.cs
@@ -0,0 +1,18 @@
+﻿using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopologyModel.TopologyGraphs
+{
+	/// <summary>
+	/// Класс ненаправленного графа топологии сети, который содержит вершины, привязанные 
+	/// к участкам предприятия, и ненаправленные взвешенные (дробным числом) ребра.
+	/// </summary>
+	public class TopologyGraph : UndirectedGraph<TopologyVertex, SUndirectedTaggedEdge<TopologyVertex, float>>
+	{
+
+	}
+}
---
+++ TopologyModel/TopologyGraphs/TopologyVertex.cs
@@ -0,0 +1,13 @@
+﻿namespace TopologyModel.TopologyGraphs
+{
+	/// <summary>
+	/// КЛасс вершины графа топологии.
+	/// </summary>
+	public class TopologyVertex
+	{
+		/// <summary>
+		/// Ссылка на участок, на котором располагается данная вершина.
+		/// </summary>
+		public Region Region { get; set; }
+	}
+}
