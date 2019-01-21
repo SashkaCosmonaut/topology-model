e02f0ac78d446447416e0267d7e4945bdacf172a
Alexander Sokolov <alexander.sokolov.it@gmail.com>
2019-01-21 21:35:21 +0400

    Добавлен конструктор для класса вершины графа топологии.

--- TopologyModel/TopologyGraphs/TopologyVertex.cs
+++ TopologyModel/TopologyGraphs/TopologyVertex.cs
@@ -1,7 +1,7 @@
 ﻿namespace TopologyModel.TopologyGraphs
 {
 	/// <summary>
-	/// КЛасс вершины графа топологии.
+	/// Класс вершины графа топологии.
 	/// </summary>
 	public class TopologyVertex
 	{
@@ -9,5 +9,14 @@
 		/// Ссылка на участок, на котором располагается данная вершина.
 		/// </summary>
 		public Region Region { get; set; }
+
+		/// <summary>
+		/// Создать вершину графа, которая находится на определённом участке предприятия.
+		/// </summary>
+		/// <param name="region">Ссылка на участкок предприятия, на котором находтся данная вершина.</param>
+		public TopologyVertex(Region region)
+		{
+			Region = region;
+		}
 	}
 }
