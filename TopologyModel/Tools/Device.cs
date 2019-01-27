7dee8ef3b66cd7db0882c4aaa649bf9ebc02b86a
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-01-27 13:50:31 +0400

    В классе устройства идентификатор теперь присваивается автоматически.

--- TopologyModel/Tools/Device.cs
+++ TopologyModel/Tools/Device.cs
@@ -8,10 +8,15 @@ namespace TopologyModel.Tools
 	/// </summary>
 	public abstract class Device
 	{
-		/// <summary>
-		/// уникальный идентификатор устройства
-		/// </summary>
-		public uint Id { get; set; }
+        /// <summary>
+        /// Глобальный автоприращиваемый идентификатор.
+        /// </summary>
+        private static uint GlobalId = 0;
+
+        /// <summary>
+        /// уникальный идентификатор устройства
+        /// </summary>
+        public uint Id { get; }
 
 		/// <summary>
 		/// множество стандартов отправки данных, по которым данное устройство отправляет данные
@@ -50,5 +55,13 @@ namespace TopologyModel.Tools
 		/// стоимость сервисного обслуживания для замены аккумуляторной батареи 
 		/// </summary>
 		public double BatteryServicePrice { get; set; }
+
+        /// <summary>
+        /// Создать некоторое устройство.
+        /// </summary>
+        public Device()
+        {
+            Id = ++GlobalId;
+        }
 	}
 }
