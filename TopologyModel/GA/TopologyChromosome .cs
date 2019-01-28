5e02392e2d9d7348e35afa4af7d9394546d1bf02
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-01-28 21:26:59 +0400

    Добавлена заготовка метода генерации гена.

--- TopologyModel/GA/TopologyChromosome .cs
+++ TopologyModel/GA/TopologyChromosome .cs
@@ -15,6 +15,11 @@ namespace TopologyModel.GA
         public const ushort GENES_FOR_SECTION = 12;
 
         /// <summary>
+        /// Ссылка на текущий проект по генерации топологии сети.
+        /// </summary>
+        public Project CurrentProject { get; set; }
+
+        /// <summary>
         /// Декодировать из хромосомы прелагаемую топологию сети (фенотип), состоящую из секций для каждого
         /// места учёта и управления, соединенного с УСПД через два или менее приемопередатчика.
         /// </summary>
@@ -32,11 +37,41 @@ namespace TopologyModel.GA
         /// <param name="project">Проект по генерации топологии сети.</param>
         public TopologyChromosome(Project project) : base(project.MCZs.Length * GENES_FOR_SECTION)
         {
-            
+            for (var i = 0; i < GENES_FOR_SECTION; i++)
+                ReplaceGene(i, GenerateGene(i));
         }
 
-        public override IChromosome CreateNew() => throw new System.NotImplementedException();
+        /// <summary>
+        /// Создать новую хромосому на базе текущего проекта.
+        /// </summary>
+        /// <returns>Новый объект хромосомы.</returns>
+        public override IChromosome CreateNew()
+        {
+            return new TopologyChromosome(CurrentProject);
+        }
 
-        public override Gene GenerateGene(int geneIndex) => throw new System.NotImplementedException();
+        /// <summary>
+        /// Клонировать данную хромосому, которая ссылается на тот же объект проекта.
+        /// </summary>
+        /// <returns>Новый объект-клон текущей хромосомы.</returns>
+        public override IChromosome Clone()
+        {
+            var clone = base.Clone() as TopologyChromosome;
+
+            clone.CurrentProject = CurrentProject;
+
+            return clone;
+        }
+
+        /// <summary>
+        /// Генерировать новый ген для хромосомы.
+        /// </summary>
+        /// <param name="geneIndex">Индекс гена.</param>
+        /// <returns>Новый объект гена.</returns>
+        public override Gene GenerateGene(int geneIndex)
+        {
+            // Создать ген, опираясь от порядкового номера
+            return new Gene();
+        }
     }
 }
