b51b3542a4c4ea79c991b40e435ad42d1526a4cc
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-01-29 21:23:46 +0400

    Добавлен метод проверки, подходит ли устройство для места учёта и управления.

--- TopologyModel/Tools/MeasurementAndControlDevice.cs
+++ TopologyModel/Tools/MeasurementAndControlDevice.cs
@@ -1,4 +1,7 @@
-﻿using TopologyModel.Enumerations;
+﻿using System;
+using System.Linq;
+using TopologyModel.Enumerations;
+using TopologyModel.Regions;
 
 namespace TopologyModel.Tools
 {
@@ -15,6 +18,46 @@ namespace TopologyModel.Tools
 		/// <summary>
 		/// множество всех управляющих воздействий, позволяющих осуществлять данным КУ
 		/// </summary>
-		public Control[] Сontrols { get; set; }
+		public Control[] Controls { get; set; }
+
+        /// <summary>
+        /// Проверить, подходит ли данное устройство для места учёта и управления - покрывает ли оно 
+        /// те измерения и управляющиее воздействия, которые необходимы на месте учёта.
+        /// На данный момент сделано допущение, что устройство покрывает более или столько же измерений,
+        /// или управляющих воздействий, сколько требуется на месте учёта и управления.
+        /// </summary>
+        /// <param name="mcz">Место учёта и управления.</param>
+        /// <returns>True, если подходит.</returns>
+        public bool IsSuitableForMCZ(MeasurementAndControlZone mcz)
+        {
+            try
+            {
+                // Вначале вобще выбираем, что необходимо ли измерять или управлять на месте
+                var measurementsResult = mcz.Measurements != null && mcz.Measurements.Any();
+                var controlsResult = mcz.Controls != null && mcz.Controls.Any();
+
+                if (measurementsResult)
+                    foreach (var m in mcz.Measurements.TakeWhile(q => measurementsResult))
+                        measurementsResult &= Measurements.Contains(m);     // Если на месте есть измерение, которое не покрывается устройством, прерываем проверку
+                else
+                    measurementsResult = true;      // Если нет измерений на месте, то устройство автоматически подходит
+
+
+                // Аналогично с управляющими воздействиями
+                if (controlsResult)
+                    foreach (var c in mcz.Controls.TakeWhile(q => controlsResult))
+                        controlsResult &= Controls.Contains(c);
+                else
+                    controlsResult = true;
+
+                return measurementsResult && controlsResult;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("IsSuitableForMCZ failed! {0}", ex.Message);
+
+                return false;
+            }
+        }
 	}
 }
