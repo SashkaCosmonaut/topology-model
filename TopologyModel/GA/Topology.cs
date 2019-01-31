4ec84fc163944b7b42da56c8bade793580dd047b
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-01-31 20:22:09 +0400

    Добавлен класс предлагаемой топологии.

---
+++ TopologyModel/GA/Topology.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using TopologyModel.TopologyGraphs;
+
+namespace TopologyModel.GA
+{
+    /// <summary>
+    /// Класс предлагаемой топологии - результат работы метода.
+    /// </summary>
+    public class Topology
+    {
+        /// <summary>
+        /// Секции топологии.
+        /// </summary>
+        public TopologySection[] Sections { get; set; }
+
+        /// <summary>
+        /// Связи между элементами секций.
+        /// </summary>
+        public IEnumerable<TopologyEdge> Pathes { get; set; }
+    }
+}
