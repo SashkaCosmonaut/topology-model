615d9c566f9f02ae75eb1a1f5b47b62e13a25d9d
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-01 16:08:17 +0400

    Добавлены словесные алгоритмы дальнейших рассчётов.

--- TopologyModel/GA/TopologyFitness.cs
+++ TopologyModel/GA/TopologyFitness.cs
@@ -1,6 +1,7 @@
 ﻿using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Fitnesses;
 using System;
+using System.Linq;
 
 namespace TopologyModel.GA
 {
@@ -21,6 +22,22 @@ namespace TopologyModel.GA
                 var topologyChromosome = chromosome as TopologyChromosome;
 
                 if (topologyChromosome == null) return 0;
+
+                var topology = topologyChromosome.Decode();
+
+                // Надо сделать режимы, по какому критерию считаются стоимости - по деньгам и по времени, надо добавить в проект поле 
+                        
+                // Группируем секции по частям с УСПД и перебираем группы
+                foreach (var dadGroup in topology.Sections.GroupBy(q => q.DADPart))
+                {
+                    // Рассчитываем стоимость денег или времени установки и обслуживания всех счётчиков в группе
+                    // Рассчитываем стоимость денег или времени  установки и обслуживания УСПД
+                    // Находим путь между УСПД и всеми счётчиками по указанной в канале топологии
+                    // Рассчитываем стоимость денег или времени пути
+                }
+
+                // Рассчитываем общую сумму всех полученных выше значений плюс затраты на эксплуатацию во времени 
+                // Значение общей стоимости и будет результатом фитнес функции
 
                 return 0;
             }
