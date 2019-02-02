20f8833b31735184703c6f402b6b7ec95f7469b0
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-02 18:21:34 +0400

    Добавлено перечисление месторасположений внутри участка.

---
+++ TopologyModel/Enumerations/LocationInRegion.cs
@@ -0,0 +1,26 @@
+﻿namespace TopologyModel.Enumerations
+{
+    /// <summary>
+    /// Перечисление месторасположений внутри участка.
+    /// </summary>
+    public enum LocationInRegion
+    {
+        TopBorder,
+
+        RightBorder,
+
+        BottomBorder,
+
+        LeftBorder,
+
+        LeftTopCorder,
+
+        RightTopCorner,
+
+        RightBottomCorner,
+
+        LeftBottomCorner,
+
+        Inside
+    }
+}
