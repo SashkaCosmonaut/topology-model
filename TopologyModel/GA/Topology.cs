5cde0974f037b19bbbf9f83a23fd451c4823e81e
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-03 14:31:28 +0400

    Код нахождения путей топологии вынесен в класс фенотипа хромосомы.

--- TopologyModel/GA/Topology.cs
+++ TopologyModel/GA/Topology.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TopologyModel.Tools;
 using TopologyModel.TopologyGraphs;
 
 namespace TopologyModel.GA
 {
     /// <summary>
-    /// Класс предлагаемой топологии - результат работы метода.
+    /// Класс предлагаемой топологии - результат работы метода, фенотип хромосомы.
     /// </summary>
     public class Topology
     {
@@ -16,10 +17,10 @@ namespace TopologyModel.GA
         public TopologySection[] Sections { get; }
 
         /// <summary>
-        /// Словарь связей между элементами одной и более секций, где ключ - УСПД, 
-        /// из которого исходит связь, а значение - перечисление граней графа связи.
+        /// Словарь связей между элементами одной и более секций, где ключ - УСПД, из которого исходит связь, а значение - также словарь, 
+        /// в котором ключ - канал передачи, соединяющий УСПД и КУ, а значение - перечисление граней графа связи, в которых находятся КУ.
         /// </summary>
-        public Dictionary<DataAcquisitionDevice, IEnumerable<TopologyEdge>> Pathes { get; }
+        public Dictionary<DataAcquisitionDevice, Dictionary<DataChannel, IEnumerable<TopologyEdge>>> Pathes { get; }
 
         /// <summary>
         /// Создать новую топологию - декодировать фенотип хромосомы на базе её текущего генотипа.
@@ -30,8 +31,10 @@ namespace TopologyModel.GA
             try
             {
                 Sections = new TopologySection[chromosome.CurrentProject.MCZs.Length];
+                Pathes = new Dictionary<DataAcquisitionDevice, Dictionary<DataChannel, IEnumerable<TopologyEdge>>>();
 
                 DecodeSections(chromosome);
+                DecodePathes(chromosome);
             }
             catch (Exception ex)
             {
@@ -58,6 +61,37 @@ namespace TopologyModel.GA
             catch (Exception ex)
             {
                 Console.WriteLine("DecodeSections failed! {0}", ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Декодировать пути между элементами секций хромосомы.
+        /// </summary>
+        /// <param name="chromosome">Декодируемая хромосома.</param>
+        protected void DecodePathes(TopologyChromosome chromosome)
+        {
+            try
+            {
+                foreach (var dadGroup in Sections.GroupBy(q => q.DADPart))                         // Группируем секции по частям с УСПД и перебираем группы
+                {
+                    foreach (var channelGroup in dadGroup.GroupBy(q => q.Channel))                          // Группы секций группируем по каналам передачи данных, которые связывают КУ и УСПД 
+                    {
+                        if (channelGroup.Count() > channelGroup.Key.MaxDevicesConnected)                    // Если в группе больше устройств, чам можно подключить по КПД
+                        {
+                         //   return Double.MaxValue;                                                         // С помощью данной хромосомы нельзя соединить устройства, прерываем поиск
+                        }
+                        
+                        // Находим путь между УСПД и всеми счётчиками по каждому каналу передачи в группе секций
+                        var path = TopologyPathfinder.SectionShortestPath(chromosome.CurrentProject.Graph, dadGroup.Key.Vertex, channelGroup.Select(q => q.MACPart.Vertex), channelGroup.Key);
+
+                        // Рассчитываем стоимость денег или времени пути
+                        // TODO: Надо учитывать режимы, по какому критерию считаются стоимости - по деньгам и по времени
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DecodePathes failed! {0}", ex.Message);
             }
         }
     }
--- TopologyModel/GA/TopologyFitness.cs
+++ TopologyModel/GA/TopologyFitness.cs
@@ -30,23 +30,12 @@ namespace TopologyModel.GA
 
                 foreach (var dadGroup in topology.Sections.GroupBy(q => q.DADPart))                         // Группируем секции по частям с УСПД и перебираем группы
                 {
+                    fitness += dadGroup.Key.GetCost(topologyChromosome.CurrentProject);                     // Рассчитываем целевую стоимость УСПД в группе
                     fitness += dadGroup.Sum(q => q.MACPart.GetCost(topologyChromosome.CurrentProject));     // Рассчитываем целевую стоимость всех КУ в группе
-                    fitness += dadGroup.Sum(q => q.DADPart.GetCost(topologyChromosome.CurrentProject));     // Рассчитываем целевую стоимость всех УСПД в группе
-
-                    foreach (var channelGroup in dadGroup.GroupBy(q => q.Channel))                          // Группы секций группируем по каналам передачи данных, которые связывают КУ и УСПД 
-                    {
-                        if (channelGroup.Count() > channelGroup.Key.MaxDevicesConnected)                    // Если в группе больше устройств, чам можно подключить по КПД
-                            return Double.MaxValue;                                                         // С помощью данной хромосомы нельзя соединить устройства, прерываем поиск
-
-                        // Находим путь между УСПД и всеми счётчиками по каждому каналу передачи в группе секций
-                        var path = TopologyPathfinder.SectionShortestPath(topologyChromosome.CurrentProject.Graph, dadGroup.Key.Vertex, channelGroup.Select(q => q.MACPart.Vertex), channelGroup.Key);
-
-                        // Рассчитываем стоимость денег или времени пути
-                        // TODO: Надо учитывать режимы, по какому критерию считаются стоимости - по деньгам и по времени
-                    }
                 }
 
                 // Рассчитываем общую сумму всех полученных выше значений плюс затраты на эксплуатацию во времени 
+                // topology.Pathes;
 
                 return fitness;     // Значение общей стоимости и будет результатом фитнес функции
             }
