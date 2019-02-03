964c62785f7b6a77b0ae3016389b4a8be81e15a9
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-03 14:01:21 +0400

    Частично реализована фитнес-функция, без расчёта путей.

--- TopologyModel/GA/TopologyFitness.cs
+++ TopologyModel/GA/TopologyFitness.cs
@@ -2,6 +2,7 @@
 using GeneticSharp.Domain.Fitnesses;
 using System;
 using System.Linq;
+using TopologyModel.TopologyGraphs;
 
 namespace TopologyModel.GA
 {
@@ -25,21 +26,29 @@ namespace TopologyModel.GA
 
                 var topology = topologyChromosome.Decode();
 
-                // Надо сделать режимы, по какому критерию считаются стоимости - по деньгам и по времени, надо добавить в проект поле 
-                        
-                // Группируем секции по частям с УСПД и перебираем группы
-                foreach (var dadGroup in topology.Sections.GroupBy(q => q.DADPart))
+                var fitness = 0.0;  // Результирующее значение функции
+
+                foreach (var dadGroup in topology.Sections.GroupBy(q => q.DADPart))                         // Группируем секции по частям с УСПД и перебираем группы
                 {
-                    // Рассчитываем стоимость денег или времени установки и обслуживания всех счётчиков в группе
-                    // Рассчитываем стоимость денег или времени  установки и обслуживания УСПД
-                    // Находим путь между УСПД и всеми счётчиками по указанной в канале топологии
-                    // Рассчитываем стоимость денег или времени пути
+                    fitness += dadGroup.Sum(q => q.MACPart.GetCost(topologyChromosome.CurrentProject));     // Рассчитываем целевую стоимость всех КУ в группе
+                    fitness += dadGroup.Sum(q => q.DADPart.GetCost(topologyChromosome.CurrentProject));     // Рассчитываем целевую стоимость всех УСПД в группе
+
+                    foreach (var channelGroup in dadGroup.GroupBy(q => q.Channel))                          // Группы секций группируем по каналам передачи данных, которые связывают КУ и УСПД 
+                    {
+                        if (channelGroup.Count() > channelGroup.Key.MaxDevicesConnected)                    // Если в группе больше устройств, чам можно подключить по КПД
+                            return Double.MaxValue;                                                         // С помощью данной хромосомы нельзя соединить устройства, прерываем поиск
+
+                        // Находим путь между УСПД и всеми счётчиками по каждому каналу передачи в группе секций
+                        var path = TopologyPathfinder.SectionShortestPath(topologyChromosome.CurrentProject.Graph, dadGroup.Key.Vertex, channelGroup.Select(q => q.MACPart.Vertex), channelGroup.Key);
+
+                        // Рассчитываем стоимость денег или времени пути
+                        // TODO: Надо учитывать режимы, по какому критерию считаются стоимости - по деньгам и по времени
+                    }
                 }
 
                 // Рассчитываем общую сумму всех полученных выше значений плюс затраты на эксплуатацию во времени 
-                // Значение общей стоимости и будет результатом фитнес функции
 
-                return 0;
+                return fitness;     // Значение общей стоимости и будет результатом фитнес функции
             }
             catch (Exception ex)
             {
