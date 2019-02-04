081626bfb123353fa72a69150b74d9565cb6774b
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-04 18:19:47 +0400

    Свойства переименованы.

--- TopologyModel/Tools/DataAcquisitionDevice.cs
+++ TopologyModel/Tools/DataAcquisitionDevice.cs
@@ -12,12 +12,12 @@ namespace TopologyModel.Tools
         /// Множество доступных стандартов приёма данных и максимальное количество подключаемых устройств 
         /// TODO: в фитнес-функции нужно учитывать количество подключаемых устройств
         /// </summary>
-        public Dictionary<DataChannelCommunication, int> ReceivingProtocols { get; set; }
+        public Dictionary<DataChannelCommunication, int> ReceivingCommunications { get; set; }
 
         /// <summary>
         /// Множество доступных способов передачи данных на сервер
         /// TODO: в фитнес функции надо учитывать, подходит ли устройство участку
         /// </summary>
-        public InternetConnection[] ServerSendingProtocols { get; set; }
+        public InternetConnection[] ServerConnections { get; set; }
     }
 }
