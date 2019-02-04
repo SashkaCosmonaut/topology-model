7db342e6639d238b771112191941a4c07a027e8f
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-04 13:45:02 +0400

    Переименован класс участка объекта предприятия и ряд его полей.

--- TopologyModel/Regions/FacilityRegion.cs
+++ TopologyModel/Regions/FacilityRegion.cs
@@ -7,48 +7,53 @@ namespace TopologyModel.Regions
     /// <summary>
     /// Класс участка объекта предприятия.
     /// </summary>
-    public class TopologyRegion : AbstractRegion
+    public class FacilityRegion : AbstractRegion
     {
         /// <summary>
-        /// Максимальное количество оценок в массивах оценок.
+        /// Длина массивов оценок свойств участка.
         /// </summary>
-        public const ushort MAX_NUMBER_OF_ESTIMATES = 5;
+        public const ushort ESTIMATES_ARRAY_LENGTH = 5;
+
+        #region EquipmentProperties
 
         /// <summary>
-        /// наличие на данном участке проводного подключения к локальной сети предприятия 
+        /// Наличие на данном участке проводного подключения к локальной сети предприятия 
         /// </summary>
         public bool HasLan { get; set; }
 
-		/// <summary>
-		/// экспертная оценка недоступности (слабости сигнала) на данном участке беспроводного подключения к локальной сети объекта предприятия
-		/// </summary>
-		public bool HasWiFi { get; set; }
+        /// <summary>
+        /// Наличие на данном участке беспроводного подключения к локальной сети объекта предприятия
+        /// </summary>
+        public bool HasWiFi { get; set; }
 
-		/// <summary>
-		/// наличие на данном участке питающей электрической сети 220 В
-		/// </summary>
-		public bool HasPower { get; set; }
+        /// <summary>
+        /// Наличие на данном участке питающей электрической сети 220 В
+        /// </summary>
+        public bool HasPower { get; set; }
 
-		/// <summary>
-		/// экспертная оценка доступности на данном участке различных радиоволн извне участка
-		/// </summary>
-		public ushort MobileInternetSignalEstimate { get; set; }    // Вес проводной связи сквозь стену
+        /// <summary>
+        /// Экспертная оценка доступности на данном участке различных радиоволн извне участка
+        /// </summary>
+        public ushort BadMobileInternetSignal { get; set; }    // Вес проводной связи сквозь стену
 
+        #endregion
 
-		/// <summary>
-		/// Экспертная оценка агрессивности окружающей среды на стенах участка и внутри участка: сверху, справа, снизу, слева, внутри
-		/// </summary>
-		public ushort[] Aggressiveness { get; set; }
+        #region EstimatesArrays
 
-		/// <summary>
-		/// Экспертная оценка непригодности стен участка для монтажа оборудования и внутри: сверху, справа, снизу, слева, внутри
-		/// </summary>
-		public ushort[] Unavailability { get; set; }
+        /// <summary>
+        /// Экспертная оценка агрессивности окружающей среды на стенах участка и внутри участка: сверху, справа, снизу, слева, внутри
+        /// </summary>
+        public ushort[] Aggressiveness { get; set; }
 
-		/// <summary>
-		/// Экспертная оценка трудоемкости проведения монтажных работ на стенах участка и внутри участка: сверху, справа, снизу, слева, внутри
-		/// </summary>
-		public ushort[] Laboriousness { get; set; }
+        /// <summary>
+        /// Экспертная оценка непригодности стен участка для монтажа оборудования и внутри: сверху, справа, снизу, слева, внутри
+        /// </summary>
+        public ushort[] Unavailability { get; set; }
+
+        /// <summary>
+        /// Экспертная оценка трудоемкости проведения монтажных работ на стенах участка и внутри участка: сверху, справа, снизу, слева, внутри
+        /// </summary>
+        public ushort[] Laboriousness { get; set; }
 
         /// <summary>
         /// Экспертная оценка непроходимости радиоволн через стены участка и внутри участка: сверху, справа, снизу, слева, внутри
@@ -60,6 +65,8 @@ namespace TopologyModel.Regions
         /// </summary>
         public ushort[] BadWiredTransmittance { get; set; }
 
+        #endregion
+
         /// <summary>
         /// Получить информацию об основных свойствах участка.
         /// </summary>
@@ -69,13 +76,15 @@ namespace TopologyModel.Regions
             return JsonConvert.SerializeObject(new
             {
                 Name,
-                Equipment = new {
+                Equipment = new
+                {
                     HasLan,
                     HasWiFi,
                     HasPower,
-                    Mobile = MobileInternetSignalEstimate,
+                    MobileSignal = BadMobileInternetSignal,
                 },
-                Estimates = new {
+                Estimates = new
+                {
                     Aggressiveness,
                     Unavailability,
                     Laboriousness,
@@ -146,12 +155,12 @@ namespace TopologyModel.Regions
             try
             {
                 if (estimates == null || estimates.Length == 0)
-                    throw new ArgumentNullException(nameof(estimates), "Incorrect size of estimates array!");
+                    throw new ArgumentNullException(nameof(estimates), "Estimates array is null or empty!");
 
                 if (location == LocationInRegion.Inside)                // Для внутренней части участка - последняя оценка 
-                    return GetEstimateFromArray(estimates, MAX_NUMBER_OF_ESTIMATES - 1);
+                    return GetEstimateFromArray(estimates, ESTIMATES_ARRAY_LENGTH - 1);
 
-                if ((int)location < MAX_NUMBER_OF_ESTIMATES - 1)        // Если требуется не угловая оценка, берём просто по индексу в массиве оценок 
+                if ((int)location < ESTIMATES_ARRAY_LENGTH - 1)         // Если требуется не угловая оценка, берём просто по индексу в массиве оценок 
                     return GetEstimateFromArray(estimates, (int)location);
 
                 switch (location)       // Для угловых месторасположений считаем среднее по двум границам
@@ -185,15 +194,15 @@ namespace TopologyModel.Regions
         /// Получить экспертную оценку из массива оценок в зависимости от количества оценок в массиве.
         /// </summary>
         /// <param name="estimates">Массив экспертных оценок для участка.</param>
-        /// <param name="location">Расположение внутри участка.</param>
+        /// <param name="intLocation">Расположение внутри участка в виде целого числа.</param>
         /// <returns>Значение оценки из массива.</returns>
         protected static ushort GetEstimateFromArray(ushort[] estimates, int intLocation)
         {
-            if (intLocation >= MAX_NUMBER_OF_ESTIMATES) return 0;   // Проверяем, что локация адекватна
+            if (intLocation >= ESTIMATES_ARRAY_LENGTH) return 0;    // Проверяем, что локация адекватна
 
-            return estimates.Length == MAX_NUMBER_OF_ESTIMATES  // Если в массиве все значения
-                ? estimates[intLocation]                        // Берём в зависимости от местарасположения
-                : estimates[0];                                 // Иначе берём первое значение
+            return estimates.Length == ESTIMATES_ARRAY_LENGTH       // Если в массиве все значения
+                ? estimates[intLocation]                            // Берём в зависимости от местарасположения
+                : estimates[0];                                     // Иначе берём первое значение
         }
     }
 }
