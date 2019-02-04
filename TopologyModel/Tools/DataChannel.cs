57c242a7ecd5bc69dd5b61abc9f1d9debdb73117
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-04 17:28:04 +0400

    Добавлено TODO

--- TopologyModel/Tools/DataChannel.cs
+++ TopologyModel/Tools/DataChannel.cs
@@ -1,38 +1,38 @@
 ﻿using System;
 using TopologyModel.Enumerations;
-using TopologyModel.TopologyGraphs;
+using TopologyModel.Graphs;
 
 namespace TopologyModel.Tools
 {
-	/// <summary>
-	/// Класс канала передачи данных.
-	/// </summary>
-	public class DataChannel : AbstractTool
-	{
-		/// <summary>
-		/// Является ли канал передачи данных беспроводным 
-		/// </summary>
-		public bool IsWireless { get; set; }
+    /// <summary>
+    /// Класс канала передачи данных.
+    /// </summary>
+    public class DataChannel : AbstractTool
+    {
+        /// <summary>
+        /// Является ли канал передачи данных беспроводным 
+        /// </summary>
+        public bool IsWireless { get; set; }
 
-		/// <summary>
-		/// Стандарт передачи данных, используемый в данном КПД
-		/// </summary>
-		public Protocol Protocol { get; set; }
+        /// <summary>
+        /// Стандарт передачи данных, используемый в данном КПД
+        /// </summary>
+        public DataChannelCommunication Communication { get; set; }
 
-		/// <summary>
-		/// Топология, допустимая для использования в данном КПД
-		/// </summary>
-		public DataChannelTopology Topology { get; set; }
+        /// <summary>
+        /// Топология, допустимая для использования в данном КПД
+        /// </summary>
+        public DataChannelTopology Topology { get; set; }
 
-		/// <summary>
-		/// Максимальная дальность передачи данных по данному КПД
-		/// </summary>
-		public double MaxRange { get; set; }
+        /// <summary>
+        /// Максимальная дальность передачи данных по данному КПД
+        /// </summary>
+        public double MaxRange { get; set; }
 
-		/// <summary>
-		/// Максимально допустимое количество устройств, которые могут передавать данные через данный КПД
-		/// </summary>
-		public uint MaxDevicesConnected { get; set; }
+        /// <summary>
+        /// Максимально допустимое количество устройств, которые могут передавать данные через данный КПД
+        /// </summary>
+        public uint MaxDevicesConnected { get; set; }
 
         /// <summary>
         /// Рассчитать затраты на использование данного инструмента для формирования сети.
@@ -44,6 +44,7 @@ namespace TopologyModel.Tools
         {
             try
             {
+                // TODO: реализовать
                 return 0;
             }
             catch (Exception ex)
