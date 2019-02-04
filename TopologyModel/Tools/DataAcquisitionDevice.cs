6cf0702c2c977e390af473b8167e1246f687f26c
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-04 17:24:45 +0400

    Добавлена пара TODO.

--- TopologyModel/Tools/DataAcquisitionDevice.cs
+++ TopologyModel/Tools/DataAcquisitionDevice.cs
@@ -6,16 +6,18 @@ namespace TopologyModel.Tools
     /// <summary>
     /// Класс УСПД.
     /// </summary>
-    public class DataAcquisitionDevice : AbstractDevice	
-	{
-		/// <summary>
-		/// множество доступных стандартов приёма данных и максимальное количество подключаемых устройств 
-		/// </summary>
-		public Dictionary<Protocol, int> ReceivingProtocols { get; set; }
+    public class DataAcquisitionDevice : AbstractDevice
+    {
+        /// <summary>
+        /// Множество доступных стандартов приёма данных и максимальное количество подключаемых устройств 
+        /// TODO: в фитнес-функции нужно учитывать количество подключаемых устройств
+        /// </summary>
+        public Dictionary<DataChannelCommunication, int> ReceivingProtocols { get; set; }
 
-		/// <summary>
-		/// множество доступных способов передачи данных на сервер
-		/// </summary>
-		public InternetConnection[] ServerSendingProtocols { get; set; }
+        /// <summary>
+        /// Множество доступных способов передачи данных на сервер
+        /// TODO: в фитнес функции надо учитывать, подходит ли устройство участку
+        /// </summary>
+        public InternetConnection[] ServerSendingProtocols { get; set; }
     }
 }
