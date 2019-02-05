8f98f28ee149407e804346a8e83857ae3f3d99a3
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-05 18:19:06 +0400

    Реализован автоматический запуск dot для генерации pdf-файла графа.

--- TopologyModel/Program.cs
+++ TopologyModel/Program.cs
@@ -33,11 +33,11 @@ namespace TopologyModel
 
                 if (!project.InitializeGraph()) return;
 
-                CreateDotFile(project);             // Сгенерировать исходный граф без отрисовки топологии
+                GenerateGraphFile(project);             // Сгенерировать исходный граф без отрисовки топологии
 
                 var topology = CalculateTopologyWithGA(project);
 
-                CreateDotFile(project, topology);   // Сгенерировать результирующий граф с топологией
+                GenerateGraphFile(project, topology);   // Сгенерировать результирующий граф с топологией
 
                 // если не достигли желаемых значений фитнес функции по деньгам или времени, то в зависимости от приоритета (с наименьшим, т.е. большим значением)
                 // удаляем одно из мест учёта из проекта (просто обнуляем), и говорим какое, и запускаем повторно алгоритм 
@@ -80,12 +80,14 @@ namespace TopologyModel
         /// </summary>
         /// <param name="project">Объект проекта.</param>
         /// <param name="topology">Предлагаемая методом топология.</param>
-        public static void CreateDotFile(Project project, Topology topology = null)
+        public static void GenerateGraphFile(Project project, Topology topology = null)
         {
             try
             {
                 // Везде добавляем переносы строк для красоты
-                var graphLabel = PrepareJSONForGraphviz(topology.GetInfo()).Replace("{'DAD", "\r\n\r\n{'DAD");
+                var graphLabel = topology == null 
+                    ? ""
+                    : PrepareJSONForGraphviz(topology.GetInfo() ?? "").Replace("{'DAD", "\r\n\r\n{'DAD");
 
                 graphLabel += project.Regions
                     .Select(q => PrepareJSONForGraphviz(q.GetInfo()))
@@ -102,7 +104,7 @@ namespace TopologyModel
                     : GetFilenameWithTopology(project.GraphDotFilename);
 
                 if (project.Graph.GenerateDotFile(filename, graphLabel, topology))
-                    Console.WriteLine("Check the graph dot-file in {0}", filename);
+                    GeneratePDFFile(filename);
             }
             catch (Exception ex)
             {
@@ -145,6 +147,29 @@ namespace TopologyModel
             {
                 Console.WriteLine("GetFilenameWithTopology failed! {0}", ex.Message);
                 return sourceFilename;
+            }
+        }
+
+        /// <summary>
+        /// Сгенерировать с помощью DOT pdf-файл графа.
+        /// </summary>
+        /// <param name="dotFilename">Имя dot-файла.</param>
+        protected static void GeneratePDFFile(string dotFilename)
+        {
+            try
+            {
+                Console.Write("Generate the graph pdf file... ");
+
+                var fullDotFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dotFilename);
+                var fullPdfFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dotFilename.Replace("dot", "pdf"));
+
+                System.Diagnostics.Process.Start("dot", $"-Tpdf {fullDotFilename} -o {fullPdfFilename}");
+
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GeneratePDFFile failed! {0}", ex.Message);
             }
         }
 
