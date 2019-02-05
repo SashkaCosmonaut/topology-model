14345a28dcaea2d59ccf919949d6065376b51bec
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-05 15:21:22 +0400

    Реализован метод расчета затрат на использование УСПД.

--- TopologyModel/Equipments/AbstractDevice.cs
+++ TopologyModel/Equipments/AbstractDevice.cs
@@ -31,12 +31,12 @@ namespace TopologyModel.Equipments
         public double BatteryServicePrice { get; set; }
 
         /// <summary>
-        /// Рассчитать затраты на использование данного инструмента для формирования сети.
+        /// Рассчитать базовые затраты на использование данного инструмента для формирования сети.
         /// </summary>
         /// <param name="project">Свойства проекта.</param>
         /// <param name="vertex">Вершина графа, в которой установлен инструмент.</param>
         /// <returns>Значение выбранных затрат на данный инструмент.</returns>
-        public double GetCost(Project project, TopologyVertex vertex)
+        public virtual double GetCost(Project project, TopologyVertex vertex)
         {
             try
             {
@@ -70,7 +70,7 @@ namespace TopologyModel.Equipments
             }
             catch (Exception ex)
             {
-                Console.WriteLine("MeasurementAndControlDevice GetCost failed! {0}", ex.Message);
+                Console.WriteLine("AbstractDevice GetCost failed! {0}", ex.Message);
                 return 0;
             }
         }
--- TopologyModel/Equipments/DataAcquisitionDevice.cs
+++ TopologyModel/Equipments/DataAcquisitionDevice.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TopologyModel.Enumerations;
+using TopologyModel.Graphs;
+using TopologyModel.Regions;
 
 namespace TopologyModel.Equipments
 {
@@ -19,5 +23,59 @@ namespace TopologyModel.Equipments
         /// TODO: в фитнес функции надо учитывать, подходит ли устройство участку
         /// </summary>
         public InternetConnection[] ServerConnections { get; set; }
+
+        /// <summary>
+        /// Рассчитать затраты на использование данного УСПД для формирования сети.
+        /// </summary>
+        /// <param name="project">Свойства проекта.</param>
+        /// <param name="vertex">Вершина графа, в которой установлен инструмент.</param>
+        /// <returns>Значение выбранных затрат на данный инструмент.</returns>
+        public override double GetCost(Project project, TopologyVertex vertex)
+        {
+            var cost = base.GetCost(project, vertex);
+
+            try
+            {
+                if (!CanSendViaLocalNetwork(project, vertex.Region))
+                {
+                    var availableConnections = ServerConnections.Where(q => project.MobileInternetMonthlyPayment.ContainsKey(q));
+
+                    if (!availableConnections.Any())    // Если в проекте не задано не одного тарифа мобильного Интернета, который поддерживается УСПД
+                        return cost + 999999;
+
+                    // Берём самый дешёвый способ передачи данных
+                    cost += availableConnections.Min(q => project.MobileInternetMonthlyPayment[q]) * project.UsageMonths;
+
+                    cost += PurchasePrice * vertex.Region.BadMobileInternetSignal / 10; // Учитываем затраты на плохую связь
+                }   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DataAcquisitionDevice GetCost failed! {0}", ex.Message);
+            }
+
+            return cost;
+        }
+
+        /// <summary>
+        /// Можно ли передавать данные на сервер через локальную сеть или Интернет.
+        /// </summary>
+        /// <param name="project">Свойства проекта.</param>
+        /// <param name="vertex">Вершина графа, в которой установлен инструмент.</param>
+        /// <returns>Результат проверки.</returns>
+        protected bool CanSendViaLocalNetwork(Project project, FacilityRegion region)
+        {
+            try
+            {
+                return (project.UseLocalServer || project.IsInternetAvailable) &&
+                    (region.HasLan && ServerConnections.Contains(InternetConnection.Ethernet) ||
+                     region.HasWiFi && ServerConnections.Contains(InternetConnection.WiFi));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("CanSendViaLocalNetworks failed! {0}", ex.Message);
+                return false;
+            }
+        }
     }
 }
