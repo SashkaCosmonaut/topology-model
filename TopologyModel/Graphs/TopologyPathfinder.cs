f7fd8167b2349eb902cd60ef0513b63bd71f6043
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-06 20:36:13 +0400

    Реализован поиск кратчайшего пути по топологии "Шина".

--- TopologyModel/Graphs/TopologyPathfinder.cs
+++ TopologyModel/Graphs/TopologyPathfinder.cs
@@ -73,12 +73,12 @@ namespace TopologyModel.Graphs
         {
             try
             {
+                // Задаём результирующий контейнер путей, источник и алгоритм расчёта весов   
                 var resultPath = new List<TopologyPath>();
+                var tryGetPath = graph.ShortestPathsDijkstra((edge) => { return edge.ChooseWeight(dataChannel); }, source);
 
-                // Задаём источник и способ расчёта весов   
-                var tryGetPath = graph.ShortestPathsDijkstra((edge) => { return dataChannel.IsWireless ? edge.WirelessWeight : edge.WiredWeight; }, source);
-
-                foreach (var target in targets.Where(t => t != source)) // Пропускаем случаи, когда УСПД и КУ находятся в одной вершине
+                // TODO: Пропускаем случаи, когда УСПД и КУ находятся в одной вершине
+                foreach (var target in targets)          // Для звезды находим пути из источника ко всем целям
                 {
                     tryGetPath(target, out var path);
 
@@ -110,7 +110,50 @@ namespace TopologyModel.Graphs
         /// <returns>Перечисление путей в графе.</returns>
         private static IEnumerable<TopologyPath> BusShortestPath(TopologyGraph graph, TopologyVertex source, IEnumerable<TopologyVertex> targets, DataChannel dataChannel)
         {
-            return null;
+            try
+            {
+                var bus = new List<TopologyPath>();
+                var busPartCandidates = new Dictionary<TopologyVertex, IEnumerable<TopologyEdge>>();
+                var busPartSource = source;
+
+                for (var i = 0; i < targets.Count(); i++)   // Пока не переберём все вершины целей
+                {
+                    // Перебираем те цели, которые ещё не уложили в шину
+                    foreach (var target in targets.Where(q => !bus.Select(w => w.Target).Contains(q)))
+                    {
+                        var tryGetPath = graph.ShortestPathsDijkstra((edge) => { return edge.ChooseWeight(dataChannel); }, busPartSource);
+
+                        tryGetPath(target, out var path);   // Ищем кратчайший путь из источника в цель и сохраняем его как кандидат на кратчайший
+
+                        busPartCandidates.Add(target, path);
+                    }
+
+                    // Находим минимальный вес среди кандидатов, причём, если источник и цель в одной вершине - вес нулевой
+                    var minWeight = busPartCandidates.Min(candidate => candidate.Value?.Sum(edge => edge.ChooseWeight(dataChannel)) ?? 0);
+
+                    // Определяем путь с минимальным весом и добавляем в шину
+                    var shortestPath = busPartCandidates.FirstOrDefault(candidate => (candidate.Value?.Sum(edge => edge.ChooseWeight(dataChannel)) ?? 0) == minWeight);
+
+                    bus.Add(new TopologyPath
+                    {
+                        DataChannel = dataChannel,
+                        Path = shortestPath.Value,
+                        Source = busPartSource,
+                        Target = shortestPath.Key
+                    });
+
+                    // Ищем дальше путь из цели лучшего кандидата и ищем кандидатов заново
+                    busPartSource = shortestPath.Key;
+                    busPartCandidates.Clear();
+                }
+
+                return bus;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("BusShortestPath failed! {0}", ex.Message);
+                return null;
+            }
         }
     }
 }
