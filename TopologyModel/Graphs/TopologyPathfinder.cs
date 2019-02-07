6022d4f1145e254580ca2990e64fafea1814531c
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-07 12:53:26 +0400

    Реализован поис кратчайшего пути по топологии mesh.

--- TopologyModel/Graphs/TopologyPathfinder.cs
+++ TopologyModel/Graphs/TopologyPathfinder.cs
@@ -58,7 +58,60 @@ namespace TopologyModel.Graphs
         /// <returns>Перечисление путей в графе.</returns>
         private static IEnumerable<TopologyPath> MeshShortestPath(TopologyGraph graph, TopologyVertex source, IEnumerable<TopologyVertex> targets, DataChannel dataChannel)
         {
-            return null;
+            try
+            {
+                // Кандидаты составных частей сети, где ключ - пара источника и целевой вершины, а значение - путь между ними
+                var meshPartCandidates = new Dictionary<KeyValuePair<TopologyVertex, TopologyVertex>, IEnumerable<TopologyEdge>>();
+                var mesh = new List<TopologyPath>();                            // Итоговая сеть смешанной топологии
+                var meshPartSources = new List<TopologyVertex> { source };      // Все возможные источники сети
+                var remainedTargets = new List<TopologyVertex>(targets);        // Оставшиеся целевые вершины
+
+                for (var i = 0; i < targets.Count(); i++)           // Пока не переберём все целевые вершины
+                {
+                    foreach (var partSource in meshPartSources)     // Перебираем все имеющиеся источники сети
+                    {
+                        foreach (var target in remainedTargets)     // Перебираем все оставшиеся целевые вершины
+                        {
+                            var tryGetPath = graph.ShortestPathsDijkstra((edge) => { return edge.ChooseWeight(dataChannel); }, partSource);
+
+                            tryGetPath(target, out var path);       // Ищем кратчайший путь из источника в цель и сохраняем его как кандидат на кратчайший
+
+                            meshPartCandidates.Add(new KeyValuePair<TopologyVertex, TopologyVertex>(partSource, target), path);
+                        }
+                    }
+
+                    // Находим минимальный вес среди кандидатов, причём, если источник и цель в одной вершине - вес нулевой
+                    var minWeight = meshPartCandidates.Min(candidate => candidate.Value?.Sum(edge => edge.ChooseWeight(dataChannel)) ?? 0);
+
+                    // Определяем путь с минимальным весом и добавляем в шину
+                    var shortestPath = meshPartCandidates.FirstOrDefault(candidate => (candidate.Value?.Sum(edge => edge.ChooseWeight(dataChannel)) ?? 0) == minWeight);
+
+                    var shortestPathSource = shortestPath.Key.Key;      // Источник находится в ключе пары найденного пути, где сам является ключем
+                    var shortestPathTarget = shortestPath.Key.Value;    // Целевая вершина находится в ключе пары найденного пути, где является значением
+
+                    mesh.Add(new TopologyPath
+                    {
+                        DataChannel = dataChannel,
+                        Path = shortestPath.Value,      
+                        Source = shortestPathSource,
+                        Target = shortestPathTarget
+                    });
+
+                    // Вершина-источник найденной части сети тоже теперь может являться источником, если ещё не является
+                    if (!meshPartSources.Contains(shortestPathSource))
+                        meshPartSources.Add(shortestPathSource);
+
+                    remainedTargets.Remove(shortestPathTarget);     // Больше не рассматриваем целевую вершину из найденной части сети
+                    meshPartCandidates.Clear();                     // Ищем кандидатов заново
+                }
+
+                return mesh;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("BusShortestPath failed! {0}", ex.Message);
+                return null;
+            }
         }
 
         /// <summary>
@@ -112,18 +165,18 @@ namespace TopologyModel.Graphs
         {
             try
             {
-                var bus = new List<TopologyPath>();
-                var busPartCandidates = new Dictionary<TopologyVertex, IEnumerable<TopologyEdge>>();
-                var busPartSource = source;
+                var bus = new List<TopologyPath>();                                                     // Итоговая шина, состоящая из составных частей - путей между вершинами шины
+                var busPartCandidates = new Dictionary<TopologyVertex, IEnumerable<TopologyEdge>>();    // Кандидаты на составные части шины, где ключ - целевая вершина, значение - путь до неё
+                var busPartSource = source;                                                             // Текущая вершина-источник части шины
+                var remainedTargets = new List<TopologyVertex>(targets);                                // Оставшиеся целевые вершины
 
-                for (var i = 0; i < targets.Count(); i++)   // Пока не переберём все вершины целей
+                for (var i = 0; i < targets.Count(); i++)       // Пока не переберём все целевые вершины
                 {
-                    // Перебираем те цели, которые ещё не уложили в шину
-                    foreach (var target in targets.Where(q => !bus.Select(w => w.Target).Contains(q)))
+                    foreach (var target in remainedTargets)     // Перебираем все оставшиеся целевые вершины
                     {
                         var tryGetPath = graph.ShortestPathsDijkstra((edge) => { return edge.ChooseWeight(dataChannel); }, busPartSource);
 
-                        tryGetPath(target, out var path);   // Ищем кратчайший путь из источника в цель и сохраняем его как кандидат на кратчайший
+                        tryGetPath(target, out var path);       // Ищем кратчайший путь из источника в цель и сохраняем его как кандидат на кратчайший
 
                         busPartCandidates.Add(target, path);
                     }
@@ -133,18 +186,19 @@ namespace TopologyModel.Graphs
 
                     // Определяем путь с минимальным весом и добавляем в шину
                     var shortestPath = busPartCandidates.FirstOrDefault(candidate => (candidate.Value?.Sum(edge => edge.ChooseWeight(dataChannel)) ?? 0) == minWeight);
+                    var shortestPathTarget = shortestPath.Key;    // Целевая вершина находится в ключе найденной пары пути
 
                     bus.Add(new TopologyPath
                     {
                         DataChannel = dataChannel,
                         Path = shortestPath.Value,
                         Source = busPartSource,
-                        Target = shortestPath.Key
+                        Target = shortestPathTarget
                     });
 
-                    // Ищем дальше путь из цели лучшего кандидата и ищем кандидатов заново
-                    busPartSource = shortestPath.Key;
-                    busPartCandidates.Clear();
+                    busPartSource = shortestPathTarget;             // Целевая вершина найденного пути теперь источник    
+                    remainedTargets.Remove(shortestPathTarget);     // Убираем эту вершину из оставшихся целевых вершин
+                    busPartCandidates.Clear();                      // Ищем кандидатов заново
                 }
 
                 return bus;
