7b66c8aaeddf6efa186ee389d90bb47077f4c3d6
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-02-16 13:10:29 +0400

    Добавлено перечисление типов соединения при передачи данных в канале передачи данных.

---
+++ TopologyModel/Enumerations/ConnectionType.cs
@@ -0,0 +1,14 @@
+﻿namespace TopologyModel.Enumerations
+{
+    /// <summary>
+    /// Тип соединения при передачи данных в канале передачи данных.
+    /// </summary>
+    public enum ConnectionType
+    {
+        Wired,
+
+        Wireless,
+
+        None
+    }
+}
