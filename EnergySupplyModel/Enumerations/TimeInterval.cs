24091da1e9352a99223a58213fd14ada9fd8a5b4
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-07-01 20:09:03 +0400

    Добавлено еречисление интервалов времени, на которые разбивается период времени для анализа.

---
+++ EnergySupplyModel/Enumerations/TimeInterval.cs
@@ -0,0 +1,18 @@
+﻿namespace EnergySupplyModel.Enumerations
+{
+    /// <summary>
+    /// Перечисление интервалов времени, на которые разбивается период времени для анализа.
+    /// </summary>
+    public enum TimeInterval
+    {
+        Min1,
+
+        Min15,
+
+        Hour1,
+
+        Hour12,
+
+        Day1
+    }
+}
