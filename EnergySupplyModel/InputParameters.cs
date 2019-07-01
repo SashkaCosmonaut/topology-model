aece2abb41b5b873ecb49af002e74a5d7e2e93b0
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-07-01 20:15:19 +0400

    Добавлен класс входных параметров для модели.

---
+++ EnergySupplyModel/InputParameters.cs
@@ -0,0 +1,51 @@
+﻿using EnergySupplyModel.Enumerations;
+using System;
+
+namespace EnergySupplyModel
+{
+    /// <summary>
+    /// Класс входных параметров для модели.
+    /// </summary>
+    public class InputParameters
+    {
+        /// <summary>
+        /// Порог выявления проблемы.
+        /// </summary>
+        public double EpsilonP = 10;
+
+        /// <summary>
+        /// Порог выявления утечки.
+        /// </summary>
+        public double EpsilonS = 10;
+
+        /// <summary>
+        /// Порог выявления рентабельности меропритий.
+        /// </summary>
+        public double EpsilonC = 80;
+
+        /// <summary>
+        /// Стоимость энергоресурса в текущий момент времени.
+        /// </summary>
+        public double EnergyResourceCost { get; set; } = 8;
+
+        /// <summary>
+        /// Затраты на мероприятия по оптимизации данного объекта.
+        /// </summary>
+        public double ActivityCost { get; set; } = 10;
+
+        /// <summary>
+        /// Начало периода анализа.
+        /// </summary>
+        public DateTime Start { get; set; } = new DateTime(2010, 10, 10);
+
+        /// <summary>
+        /// Конец периода анализа.
+        /// </summary>
+        public DateTime End { get; set; } = new DateTime(2010, 10, 11);
+
+        /// <summary>
+        /// Шаг разбиения периода времени.
+        /// </summary>
+        public TimeInterval Step { get; set; } = TimeInterval.Hour1;
+    }
+}
--- EnergySupplyModel/Program.cs
+++ EnergySupplyModel/Program.cs
@@ -1,4 +1,5 @@
-﻿using EnergySupplyModel.Facilities;
+﻿using EnergySupplyModel.Enumerations;
+using EnergySupplyModel.Facilities;
 using System;
 
 namespace EnergySupplyModel
@@ -6,29 +7,9 @@ namespace EnergySupplyModel
     public class Program
     {
         /// <summary>
-        /// Порог выявления проблемы.
+        /// Входные параметры для модели.
         /// </summary>
-        public const double EPSILON_P = 10;
-
-        /// <summary>
-        /// Порог выявления утечки.
-        /// </summary>
-        public const double EPSILON_S = 10;
-
-        /// <summary>
-        /// Порог выявления рентабельности меропритий.
-        /// </summary>
-        public const double EPSILON_C = 80;
-
-        /// <summary>
-        /// Стоимость энергоресурса в текущий момент времени.
-        /// </summary>
-        public static double Cost { get; set; } = 8;
-
-        /// <summary>
-        /// Затраты на мероприятия по оптимизации данного объекта.
-        /// </summary>
-        public static double ActivityCost { get; set; } = 10;
+        public static InputParameters Params { get; set; } = new InputParameters();
 
         /// <summary>
         /// Главная функция программы.
@@ -80,7 +61,7 @@ namespace EnergySupplyModel
 
             var exprectedDiff = Math.Abs(measuredConsumption.Value - expectedConsumption);
 
-            Console.WriteLine($"Expected: {expectedConsumption}, Measured: {measuredConsumption.Value}, ExpectedDiff: {exprectedDiff}, Result: {exprectedDiff <= EPSILON_P}");
+            Console.WriteLine($"Expected: {expectedConsumption}, Measured: {measuredConsumption.Value}, ExpectedDiff: {exprectedDiff}, Result: {exprectedDiff <= Params.EpsilonP}");
         }
 
         /// <summary>
@@ -103,7 +84,7 @@ namespace EnergySupplyModel
 
                 var leakDiff = Math.Abs(measuredConsumption.Value - summaryConsumption.Value);
 
-                Console.WriteLine($"Summary: {summaryConsumption.Value}, Measured: {measuredConsumption.Value}, LeakDiff: {leakDiff}, Result: {leakDiff <= EPSILON_S}");
+                Console.WriteLine($"Summary: {summaryConsumption.Value}, Measured: {measuredConsumption.Value}, LeakDiff: {leakDiff}, Result: {leakDiff <= Params.EpsilonS}");
             }
         }
 
@@ -120,9 +101,9 @@ namespace EnergySupplyModel
 
             var potentialConsumption = facility.GetPotentialConsumption.Invoke();
 
-            var effectDiff = Cost * (expectedConsumption - potentialConsumption) - ActivityCost;
+            var effectDiff = Params.EnergyResourceCost * (expectedConsumption - potentialConsumption) - Params.ActivityCost;
 
-            Console.WriteLine($"Expected: {expectedConsumption}, Potential: {potentialConsumption}, EffectDiff: {effectDiff}, Result: {effectDiff <= EPSILON_C}");
+            Console.WriteLine($"Expected: {expectedConsumption}, Potential: {potentialConsumption}, EffectDiff: {effectDiff}, Result: {effectDiff <= Params.EpsilonC}");
         }
     }
 }
