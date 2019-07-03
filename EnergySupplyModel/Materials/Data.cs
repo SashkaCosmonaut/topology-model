37e1d23e6e787bc05f49a8bebd3e20d82f345451
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-07-03 14:20:49 +0400

    Добавлены классы данных.

---
+++ EnergySupplyModel/Materials/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnergySupplyModel.Materials
+{
+    /// <summary>
+    /// Класс данных о потреблении энергоресурсов.
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// Элементы данных этого блока данных.
+        /// </summary>
+        public Dictionary<DateTime, DataItem> DataItems { get; set; }
+    }
+}
---
+++ EnergySupplyModel/Materials/DataItem.cs
@@ -0,0 +1,13 @@
+﻿namespace EnergySupplyModel.Materials
+{
+    /// <summary>
+    /// Элемент данных.
+    /// </summary>
+    public class DataItem
+    {
+        /// <summary>
+        /// Значение элемента данных.
+        /// </summary>
+        public double Value { get; set; }
+    }
+}
