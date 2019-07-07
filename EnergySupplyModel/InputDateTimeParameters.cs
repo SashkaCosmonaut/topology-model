2a83b707ca11353d25795dd392cbd942732ee92e
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-07-07 11:24:43 +0400

    Добавлен класс временных параметров запроса данных.

---
+++ EnergySupplyModel/InputDateTimeParameters.cs
@@ -0,0 +1,26 @@
+﻿using EnergySupplyModel.Enumerations;
+using System;
+
+namespace EnergySupplyModel
+{
+    /// <summary>
+    /// Класс временных параметров запроса данных.
+    /// </summary>
+    public class InputDateTimeParameters
+    {
+        /// <summary>
+        /// Начало периода анализа.
+        /// </summary>
+        public DateTime Start { get; set; } = new DateTime(2010, 01, 10);
+
+        /// <summary>
+        /// Конец периода анализа.
+        /// </summary>
+        public DateTime End { get; set; } = new DateTime(2010, 01, 11);
+
+        /// <summary>
+        /// Шаг разбиения периода времени.
+        /// </summary>
+        public TimeInterval Interval { get; set; } = TimeInterval.Hour1;
+    }
+}
