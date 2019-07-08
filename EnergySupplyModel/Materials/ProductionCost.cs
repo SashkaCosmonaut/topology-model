d55744cd75aee2a99b945bb79297ed0ae28bc1ff
Alexander Sokolov <sashkacosmonaut@gmail.com>
2019-07-08 15:12:09 +0400

    Обновлен класс затрат энергоресурсов различных типов для производства единицы продукции определённого типа.

---
+++ EnergySupplyModel/Materials/ProductionCost.cs
@@ -0,0 +1,22 @@
+﻿using EnergySupplyModel.Enumerations;
+using System.Collections.Generic;
+
+namespace EnergySupplyModel.Materials
+{
+    /// <summary>
+    /// Затраты энергоресурсов различных типов для производства единицы продукции определённого типа.
+    /// </summary>
+    public class ProductionCost
+    {
+        /// <summary>
+        /// Тип (наименование) производимой продкции.
+        /// </summary>
+        public ProductType Type { get; set; }
+
+        /// <summary>
+        /// Требуемые затраты производимой продукции в виде словаря, в котором ключ - это тип
+        /// энергоресурса, а значение - объем его потребления для изготовления единицы продукции.
+        /// </summary>
+        public Dictionary<EnergyResourceType, double> EnergyResources { get; set; }
+    }
+}
